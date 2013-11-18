49a107abaf9c85b6d0f0dcf28d9c8a7950084787
OrfeasZ <orfeaz@gmail.com>
2013-11-18 03:10:44 +0200

    Implemented templates for the login dialog.
    Implemented modal dialogs.
    Implemented custom selectboxes.
    Various changes and fixes to the overall style and js.
    Updated the glyph font.

--- htdocs/assets/js/fallout.js
+++ htdocs/assets/js/fallout.js
@@ -2,8 +2,8 @@
     $(document).ready(function()
     {
         var s_Header = $('header');
-        var s_SideNav = $('nav#sideNav');
         var s_MainNav = s_Header.find('nav').eq(0);
+        var s_SideNav = $('nav#side-nav');
 
         $(window).scroll(function()
         {
@@ -11,6 +11,36 @@
 
             // Parallax scrolling on header background
             s_Header.css('background-position', 'center -' + (s_ScrollTop * 0.7) + 'px');
+
+            if (s_ScrollTop > 200)
+            {
+                if (s_SideNav.length > 0)
+                    s_SideNav.css({ position: 'fixed', top: '81px' });
+
+                s_MainNav.css({ position: 'fixed', top: '0', bottom: 'auto' });
+            }
+            else
+            {
+                if (s_SideNav.length > 0)
+                    s_SideNav.css({ position: 'static', top: 'auto' });
+
+                s_MainNav.css({ position: 'absolute', top: 'auto', bottom: '0' });
+            }
+        });
+
+
+        $('*[data-modal-id]').each(function(_, p_Element)
+        {
+            var s_ModalElement = $('#' + $(p_Element).attr('data-modal-id'));
+
+            if (s_ModalElement.length == 0)
+                return;
+
+            s_ModalElement.addClass('modal');
+
+            $(p_Element).attr('href', '#' + $(p_Element).attr('data-modal-id'));
+
+            $(p_Element).leanModal({ closeButton: '.icon-close' });
         });
     });
 })(jQuery);
---
+++ htdocs/assets/js/jQuery.gDropdown.js
@@ -0,0 +1,91 @@
+(function($) {
+
+    $(document).ready(function()
+    {
+        $('select.gDropdown').each(function(_, p_Element)
+        {
+            var s_Element = $(p_Element);
+
+            s_Element.removeClass('gDropdown').hide();
+
+            var s_Options = [];
+
+            var s_DropdownActive = false;
+
+            var s_SelectElement = $('<div class="select ' + s_Element.attr('class') + '" id="' + s_Element.attr('id') + '"></div>');
+
+            var s_ActiveItem = $('<span class="text" data-id="0"></span><span class="icon-caret-down right"></span>');
+
+            var s_ActiveItemItem = s_ActiveItem.eq(0);
+
+            console.log(s_ActiveItemItem);
+
+            var s_Choices = $('<div class="select-choices"><ul></ul></div>');
+
+            var s_ChoicesList = s_Choices.find('ul');
+
+            var s_IndexCounter = 0;
+            s_Element.find('option').each(function(_, p_Option)
+            {
+                (function(p_Index)
+                {
+                    var s_Option = $('<li data-id="' + p_Index + '">' + $(p_Option).html() + '</li>');
+                    s_Options.push(s_Option);
+
+                    s_ChoicesList.append(s_Option);
+
+                    s_Option.click(function()
+                    {
+                        // Select the actual selectbox option
+                        $(p_Option).prop('selected', true);
+
+                        var s_OldItemIndex = s_ActiveItemItem.attr('data-id');
+                        s_ActiveItemItem.attr('data-id', p_Index).html(s_Option.html());
+                        s_Option.hide();
+                        s_Options[s_OldItemIndex].show();
+
+                        // Collapse the choice selector
+                        s_DropdownActive = false;
+                        s_SelectElement.removeClass('expanded');
+                        s_Choices.hide();
+                    });
+
+                })(s_IndexCounter);
+
+                ++s_IndexCounter;
+            });
+
+            s_Options[0].hide();
+            s_ActiveItemItem.html(s_Options[0].html());
+
+            s_SelectElement.append(s_ActiveItem);
+
+            s_Element.after(s_SelectElement);
+            s_SelectElement.after(s_Choices);
+
+            s_Choices.css('position', 'absolute').css('display', 'none').css('margin-top', '-11px');
+
+            s_SelectElement.click(function()
+            {
+                if (!s_DropdownActive)
+                {
+                    s_DropdownActive = true;
+
+                    s_Choices.show();
+                    s_SelectElement.addClass('expanded');
+
+                    return false;
+                }
+
+                s_DropdownActive = false;
+
+                s_SelectElement.removeClass('expanded');
+                s_Choices.hide();
+
+                return false;
+            });
+
+        });
+    });
+
+})(jQuery);
